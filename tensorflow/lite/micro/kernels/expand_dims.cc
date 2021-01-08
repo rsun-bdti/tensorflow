3f199ec6babefb940bf9a76f369ab530414e32d1
rsun <rsun@bdti.com>
2021-01-07 16:04:42 -0800

    Copy lite/kernels/expand_dims.cc to lite/micro/kernels/expand_dims.cc

--- tensorflow/lite/micro/kernels/expand_dims.cc
+++ tensorflow/lite/micro/kernels/expand_dims.cc
@@ -1,4 +1,4 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ limitations under the License.
 
 namespace tflite {
 namespace ops {
-namespace builtin {
+namespace micro {
 namespace expand_dims {
 
 // Input indices
@@ -117,6 +117,6 @@ TfLiteRegistration* Register_EXPAND_DIMS() {
                                  expand_dims::Eval};
   return &r;
 }
-}  // namespace builtin
+}  // namespace micro
 }  // namespace ops
 }  // namespace tflite
